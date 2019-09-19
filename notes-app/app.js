2c27fecc23d50292a6594644707159cf2b9f8cab
mabirizi <mabiriziad@gmail.com>
2019-09-19 17:48:47 +0300

    Installed validator and chalk

--- notes-app/app.js
+++ notes-app/app.js
@@ -1,11 +1,18 @@
+const getNotes = require('./notes.js')
+
+const msg = getNotes()
+
+console.log(msg)
+
 //const fs = require('fs');
 
 //fs.writeFileSync('notes.txt', 'This file was ceted by Nodejs');
 
 //fs.appendFileSync('notes.txt', 'My name is Mabirizi');
 
-const add = require('./utils.js')
+// const add = require('./utils.js')
 
-const sum = add(4,-2)
+// const sum = add(4,-2)
 
-console.log(sum)
+// console.log(sum)
+
