31a8a649e4bbff64299af409a674a1635503d4b1
mabirizi <mabiriziad@gmail.com>
2019-09-19 15:33:44 +0300

    Importing and exporting my own files in node

--- notes-app/app.js
+++ notes-app/app.js
@@ -1,5 +1,11 @@
-const fs = require('fs');
+//const fs = require('fs');
 
 //fs.writeFileSync('notes.txt', 'This file was ceted by Nodejs');
 
-fs.appendFileSync('notes.txt', 'My name is Mabirizi');
+//fs.appendFileSync('notes.txt', 'My name is Mabirizi');
+
+const add = require('./utils.js')
+
+const sum = add(4,-2)
+
+console.log(sum)
---
+++ notes-app/utils.js
@@ -0,0 +1,9 @@
+console.log('this is the utils file')
+
+const name = 'Mike'
+
+const add = function(a,b){
+    return a+b
+}
+
+module.exports = add
