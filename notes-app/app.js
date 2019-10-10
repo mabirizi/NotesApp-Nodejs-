507a51247e4ceacfff652ff011f49f83c51345d6
mabirizi <mabiriziad@gmail.com>
2019-10-10 10:36:55 +0300

    parsing arguments using yards lib part 2

--- notes-app/app.js
+++ notes-app/app.js
@@ -2,21 +2,23 @@ const chalk = require('chalk')
 const yargs = require('yargs')
 const getNotes = require('./notes.js')
 
-// const command = process.argv[2]
-
-// if (command === "add") {
-//     console.log(chalk.green.inverse.bold('Adding a note'))
-// } else if (command === 'remove') {
-//     console.log(chalk.red.inverse.bold('Removing a note'))
-//     console.log(chalk.blue.inverse.bold('Reading a note'))
-//     console.log(chalk.yellow.inverse.bold('listing a note'))
-// } 
-
 yargs.command({
     command: 'add',
     describe: 'Adding a new note',
-    handler: function () {
-        console.log(chalk.green.inverse.bold('Adding a note'))
+    builder: {
+        title: {
+            describe: 'Note title',
+            demandOption: true,
+            type: "string",
+        },
+        body: {
+            describe:'message body',
+            demandOption: true,
+            type: "string",
+        }
+    },
+    handler: function (argv) {
+        console.log('Title:'+chalk.green.inverse.bold(argv.title)+'  Message:'+chalk.green.inverse.bold(argv.body))
     }
 })
 
@@ -24,8 +26,8 @@ yargs.command({
 yargs.command({
     command: 'remove',
     describe: 'Removing a note',
-    handler: function () {
-        console.log(chalk.red.inverse.bold('Removing a note'))
+    handler: function (argv) {
+        console.log(chalk.red.inverse.bold('Removing a note'), argv)
     }
 })
 
@@ -33,8 +35,8 @@ yargs.command({
 yargs.command({
     command: 'read',
     describe: 'Reading a note',
-    handler: function () {
-        console.log(chalk.blue.inverse.bold('Reading a note'))
+    handler: function (argv) {
+        console.log(chalk.blue.inverse.bold('Reading a note'), argv)
     }
 })
 
@@ -42,26 +44,9 @@ yargs.command({
 yargs.command({
     command: 'list',
     describe: 'listing a note',
-    handler: function () {
-        console.log(chalk.yellow.inverse.bold('listing a note'))
+    handler: function (argv) {
+        console.log(chalk.yellow.inverse.bold('listing a note'), argv)
     }
 })
 
-
-console.log(yargs.argv)
-// const msg = getNotes()
-
-// console.log(chalk.green.bold.bgBlue(msg) )
-
-//const fs = require('fs');
-
-//fs.writeFileSync('notes.txt', 'This file was ceted by Nodejs');
-
-//fs.appendFileSync('notes.txt', 'My name is Mabirizi');
-
-// const add = require('./utils.js')
-
-// const sum = add(4,-2)
-
-// console.log(sum)
-
+yargs.parse()
